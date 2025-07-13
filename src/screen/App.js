a2e6e5bd5bdfb83755d82a3f50a78fd1ecbedc0f
TUSHARKUCHHAL <kuchhaltushar2@gmail.com>
2025-07-13 19:35:33 +0530

    push

--- src/screen/App.js
+++ src/screen/App.js
@@ -30,8 +30,8 @@ function App() {
       <div className="svg-wrapper">
         <svg height="60" width="320" xmlns="http://www.w3.org/2000/svg">
           <rect className="shape" height="60" width="320" />
-        </svg>        
-        <motion.p variants={loadText} initial='hidden' animate='visible' className="text">Peace Jinadu-Paul</motion.p>
+        </svg>
+        <motion.p variants={loadText} initial='hidden' animate='visible' className="text">&nbsp;&nbsp;Tushar Kuchhal</motion.p>
       </div>
     </div>
     :
